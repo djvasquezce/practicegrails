38cef595472cf25978a59561126777053de24af1
Danny Javier Vasquez <dj.vasquez@uniandes.edu.co>
2023-03-25 23:27:59 -0500

    build.grade & application.grooy

--- bin/default/grails-app/init/practicegrails/BootStrap.groovy
+++
@@ -1,9 +0,0 @@
-package practicegrails
-
-class BootStrap {
-
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
-}
--- grails-app/init/practicegrails/Application.groovy
+++ grails-app/init/practicegrails/Application.groovy
@@ -2,10 +2,13 @@ package practicegrails
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration
 
 import groovy.transform.CompileStatic
 
 @CompileStatic
+@EnableAutoConfiguration(exclude = [SecurityFilterAutoConfiguration])
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
